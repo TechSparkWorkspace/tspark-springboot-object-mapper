6bb841911e05b5014278cdcec6ec9e30cc277d8f
Prasath Basuvaraj <prasath.buddie@gmail.com>
2024-12-10 13:18:06 -0500

    Initial Skeleton

---
+++ src/main/java/org/techspark/greeting/GreetingController.java
@@ -0,0 +1,12 @@
+package org.techspark.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/")
+    public String greeting() {
+        return "Greeting from TechSpark";
+    }
+}
